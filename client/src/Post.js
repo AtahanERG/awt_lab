b519cce807d2a10b21646b1e25ae169492af1cad
AtahanERG <ergurhanatahan@gmail.com>
2023-04-28 00:20:08 +0200

    Initial commit

---
+++ client/src/Post.js
@@ -0,0 +1,17 @@
+export default function Post(){
+    return(
+        <div className="post">
+        <div className="image">
+          <img src="https://www.happy.co.uk/media/2858/project-900.png" alt=""/>
+        </div>
+        <div className= "texts">
+          <h2>How Asana Can Improve Productivity and Collaboration in Your Team</h2>
+          <p className="info">
+            <a className="author">Atahan Ergurhan</a>
+            <time>2023-04-20 16:45</time>
+          </p>
+          <p className="summary">Asana is a user-friendly work management tool. Using Asana, teams or groups of people can organise their work, keep track of their progress and manage projects easily. In this blog, our IT expert Gill explains why she likes to use Asana for project management, and how Asana can help to improve your team’s productivity and collaboration.</p>
+        </div>      
+      </div>
+    )
+}
---
+++ client/src/pages/RegisterPage.js
@@ -0,0 +1,12 @@
+export default function RegisterPage() {
+    return(
+        <form action="" classNmae="register">
+            <h1>Register</h1>
+            <input type="text" placeholder="username"/>
+            <input type="email" placeholder="email" />
+            <input type="password" placeholder="password"/>
+            <input type="password" placeholder="password again" />
+            <button>Register</button>
+        </form>
+    )
+}
